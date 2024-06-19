e6be99c87b287ee7d2be0aa1299162bf8c9fd33c
happyprogfrog <happyprogfrog@gmail.com>
2024-06-20 08:46:31 +0900

    프로젝트 생성

---
+++ src/test/java/me/progfrog/idol/IdolApplicationTests.java
@@ -0,0 +1,13 @@
+package me.progfrog.idol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
